389ce071f4fa7126eb65dc4b7381f50318443669
Valentin Postnov <valpostnov@gmail.com>
2016-07-17 18:36:16 +0300

    add notification for headset plug event

--- app/src/main/java/com/postnov/android/summerschoolapp/artists/ArtistsActivity.java
+++ app/src/main/java/com/postnov/android/summerschoolapp/artists/ArtistsActivity.java
@@ -5,11 +5,19 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RemoteViews;
 
 import com.postnov.android.summerschoolapp.BuildConfig;
 import com.postnov.android.summerschoolapp.R;
@@ -17,6 +25,8 @@ import com.postnov.android.summerschoolapp.about.AboutActivity;
 
 public class ArtistsActivity extends Activity
 {
+    private final HeadsetPlugReceiver receiver = new HeadsetPlugReceiver();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,7 +36,22 @@ public class ArtistsActivity extends Activity
         if (savedInstanceState == null)
         {
             addFragment(ArtistsFragment.newInstance(), false);
+            showNotification(true);
         }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter(Intent.ACTION_HEADSET_PLUG));
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 
     public void addFragment(Fragment fragment, boolean addToBackStack)
@@ -88,5 +113,70 @@ public class ArtistsActivity extends Activity
         feedbackIntent.setData(Uri.parse("mailto:" + recipient));
 
         startActivity(Intent.createChooser(feedbackIntent, getString(R.string.support_chooser_text)));
+    }
+
+    private void showNotification(boolean show)
+    {
+        int notificationId = 1;
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (show)
+        {
+            NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this);
+            nBuilder.setSmallIcon(R.mipmap.ic_launcher);
+            nBuilder.setContentTitle(getString(R.string.headset_plug));
+            nBuilder.addAction(R.drawable.ic_music, getString(R.string.action_start_music),
+                    getNotificationIntent("ru.yandex.music"));
+            nBuilder.addAction(R.drawable.ic_radio, getString(R.string.action_start_radio),
+                    getNotificationIntent("ru.yandex.radio"));
+            notificationManager.notify(notificationId, nBuilder.build());
+        }
+        else
+        {
+            notificationManager.cancel(notificationId);
+        }
+    }
+
+    private PendingIntent getNotificationIntent(String packageName)
+    {
+        String musicUrl = "market://details?id=" + packageName;
+        String musicFullUrl = "https://play.google.com/store/apps/details?id=" + packageName;
+        String playStorePackage = "com.android.vending";
+
+        Intent intent = getPackageManager().getLaunchIntentForPackage(playStorePackage);
+
+        if (intent == null)
+        {
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(musicFullUrl));
+        }
+        else
+        {
+            intent = getPackageManager().getLaunchIntentForPackage(musicUrl);
+        }
+
+        return PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private class HeadsetPlugReceiver extends BroadcastReceiver
+    {
+        private static final int STATE_UNPLUG = 0;
+        private static final int STATE_PLUG = 1;
+        private static final String EXTRA_STATE = "state";
+
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            int state = intent.getExtras().getInt(EXTRA_STATE);
+            switch (state)
+            {
+                case STATE_PLUG:
+                    showNotification(true);
+                    break;
+
+                case STATE_UNPLUG:
+                    showNotification(false);
+                    break;
+            }
+        }
     }
 }
